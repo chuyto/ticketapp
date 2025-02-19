67e48bb0edf2f4bd264503279d59bdd069d80677
Arturo <jsanchez@aqm.mx>
2025-02-19 15:53:59 -0600

    commit 3

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -143,8 +143,12 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+
+            <x-responsive-nav-link href="{{ route('reportes.index') }}" :active="request()->routeIs('reportes.index')">
+                {{ __('Reportes') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('clientes.index') }}" :active="request()->routeIs('clientes.index')">
+                {{ __('Clientes') }}
             </x-responsive-nav-link>
         </div>
 
